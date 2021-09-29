3783d1c6e4465d05206b790d9db3acb6f42470a4
JbirdL86 <jgonzalez.wj@gmail.com>
2021-09-29 08:58:24 -0600

    Set store and functionality for adding books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,39 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
-const books = [
-  {
-    id: 1,
-    title: 'Whenever you can',
-    author: 'Michael Douglas',
-    category: 'Hollywood movies',
-  },
-  {
-    id: 2,
-    title: 'Whenever you can',
-    author: 'Michael Jackson',
-    category: 'Thriller',
-  },
-  {
-    id: 3,
-    title: 'Whenever you can',
-    author: 'Michael Chain',
-    category: 'Fiction',
-  },
-];
+const BookList = () => {
+  const books = useSelector((state) => state.books);
 
-const BookList = () => (
-  <ul>
-    {books.map((book) => (
-      <BookItem
-        key={book.id}
-        id={book.id}
-        category={book.category}
-        title={book.title}
-        author={book.author}
-      />
-    ))}
-  </ul>
-);
+  return (
+    <>
+      <ul>
+        {books.map((book) => (
+          <BookItem
+            key={book.id}
+            bookId={book.bookId}
+            title={book.title}
+            author={book.author}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default BookList;
