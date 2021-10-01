cd02c0253063c70696ebe9293317e6ad49b7bd4f
JbirdL86 <jgonzalez.wj@gmail.com>
2021-10-01 13:21:01 -0600

    Configure redux store, reducers, actions and action creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,55 +1,41 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const LOAD = 'bookstore/books/LOAD';
-const CREATE = 'bookstore/books/CREATE';
-const UPDATE = 'bookstore/books/UPDATE';
-const REMOVE = 'bookstore/books/REMOVE';
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/S3YL3BOOI8fYhfoosSMv/books/';
 
-export default function reducer(state = [], action) {
-  let newBook;
-
+export default function reducer(state = {}, action) {
   switch (action.type) {
     case (LOAD):
-      return state;
-    case (CREATE):
-      newBook = {
-        ...action.book,
-        id: uuidv4(),
-      };
-      return (
-        [
-          ...state,
-          newBook,
-        ]
-      );
-    case (UPDATE):
-      return state.map((book) => {
-        if (book.id === action.book.id) {
-          return action.book;
-        }
-        return book;
-      });
-    case (REMOVE):
-      return state.filter((book) => book.bookId !== action.bookId);
+      return action.state;
     default: return state;
   }
 }
 
-export const loadBooks = () => ({
-  type: LOAD,
-});
+export const loadBooks = () => async (dispatch) => {
+  const res = await fetch(URL);
+  const state = await res.json();
+  dispatch({ type: LOAD, state });
+};
 
-export const addBook = (book) => ({
-  type: CREATE,
-  book,
-});
+export const addBook = (book) => async (dispatch) => {
+  await fetch(URL, {
+    method: 'POST',
+    body: new URLSearchParams({
+      item_id: uuidv4(),
+      title: book.title,
+      category: book.category,
+    }),
+  });
+  const res = await fetch(URL);
+  const state = await res.json();
+  dispatch({ type: LOAD, state });
+};
 
-export const updateBook = (book) => ({
-  type: UPDATE,
-  book,
-});
-
-export const removeBook = (bookId) => ({
-  type: REMOVE,
-  bookId,
-});
+export const removeBook = (bookId) => async (dispatch) => {
+  await fetch(URL + bookId, {
+    method: 'DELETE',
+  });
+  const res = await fetch(URL);
+  const state = await res.json();
+  dispatch({ type: LOAD, state });
+};
